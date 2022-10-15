269115a832dc57f3038d52a1d6eee288f589be58
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-10-16 03:16:27 +0900

    途中まで

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,28 @@ class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('リスト一覧画面'),
+      appBar: AppBar(
+        title: const Text('リスト一覧'),
+      ),
+      body: ListView(
+        children: const <Widget>[
+          Card(
+              child: ListTile(
+            title: Text('ニンジンを買う'),
+          )),
+          Card(
+              child: ListTile(
+            title: Text('タマネギを買う'),
+          )),
+          Card(
+              child: ListTile(
+            title: Text('ジャガイモを買う'),
+          )),
+          Card(
+              child: ListTile(
+            title: Text('カレールーを買う'),
+          )),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
@@ -47,7 +67,9 @@ class TodoAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(title: const Text('リスト追加')),
+      body: Container(
+        padding: const EdgeInsets.all(64),
         child: TextButton(
           child: const Text('リスト追加画面（クリックで戻る）'),
           // ボタンをクリックした時の処理
