796dddae9ccb78da209707c67438cfdc50b3dceb
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-10-16 03:09:27 +0900

    遷移ボタン追加

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,48 @@ class MyTodoApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TodoListPage(),
+      home: TodoListPage(),
     );
   }
 }
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      body: const Center(
+        child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// リスト追加画面用Widget
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
-        child: Text('リスト'),
+        child: TextButton(
+          child: const Text('リスト追加画面（クリックで戻る）'),
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+        ),
       ),
     );
   }
